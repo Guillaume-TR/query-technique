9b9b631c44fe863712a29f601a65f7d07d4eec98
Guillaume-TR <guillaume.terrien.gt@gmail.com>
2020-06-03 12:31:46 +0200

    init api

---
+++ api/app.js
@@ -0,0 +1,10 @@
+import server from './config/server';
+import dotenv from 'dotenv';
+
+const config = dotenv.config();
+if (config.error) throw config.error;
+
+const port = process.env.SERVER_PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server started on 192.168.1.6:${port}`);
+});
