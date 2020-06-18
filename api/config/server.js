b1a19aedc72138a97f018f05ac53ad6695206259
Guillaume-TR <guillaume.terrien.gt@gmail.com>
2020-06-18 11:13:28 +0200

    allow all origin of server

--- api/config/server.js
+++ api/config/server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(bodyParser.json());
 server.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
