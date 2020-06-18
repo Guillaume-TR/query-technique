f3d74a6b060171569b7a5d0d6f8eb40859de9eb6
Guillaume-TR <guillaume.terrien.gt@gmail.com>
2020-06-18 11:10:30 +0200

    Add home page in panel admin

---
+++ web/src/components/Admin/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './Header';
+
+export default {
+  name: 'Admin',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'src/assets/styles/variables.scss';
+
+</style>
--- web/src/routes.js
+++ web/src/routes.js
@@ -2,6 +2,9 @@ import User from './components/User';
 import Home from './components/User/Home';
 import ArticleDetails from './components/User/ArticleDetails';
 
+import Admin from './components/Admin';
+import AdminHome from './components/Admin/Home';
+
 export default [
   { path: '/', component: User,
     children: [
@@ -15,6 +18,15 @@ export default [
         name: 'user-article-details',
         component: ArticleDetails
       }
+    ]
+  },
+  { path: '/admin', component: Admin,
+    children: [
+      {
+        path: '/',
+        name: 'admin-home',
+        component: AdminHome
+      },
     ]
   },
 ]
