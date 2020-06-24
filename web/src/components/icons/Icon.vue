bc35151a6213bdcbb472b7f3c90379dbbba04820
Guillaume-TR <guillaume.terrien.gt@gmail.com>
2020-06-24 11:19:36 +0200

    Add dynamic SVG icon

---
+++ web/src/components/icons/Icon.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    fill="currentColor"
+    :width="size"
+    :height="size"
+    :class="`icon icon-${name}`"
+  >
+    <component :is="name" />
+  </svg>
+</template>
+
+<script>
+import Edit from './Edit';
+import Delete from './Delete';
+
+export default {
+  components: {
+    Edit,
+    Delete
+  },
+  props: {
+    name: {
+      type: String,
+    },
+    size: {
+      type: [Number, String],
+      default: 20
+    }
+  }
+}
+</script>
